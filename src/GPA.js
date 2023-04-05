43685716881ea905176e13886238a70d0f3fa1c6
spdinesh2002 <dinesh.sp.2019.cse@rajalakshmi.edu.in>
2023-04-05 10:24:01 +0530

    Update GPA.js

--- src/GPA.js
+++ src/GPA.js
@@ -73,24 +73,30 @@ function ChildModal() {
     const handleClose = () => {
         setOpen(false);
     };
-
-    return (
-        <>
-            <Button onClick={handleOpen}>Open Child Modal</Button>
-            <Modal
+   
+    const modal = (props) => {
+        <Modal
                 open={open}
                 onClose={handleClose}
                 aria-labelledby="child-modal-title"
                 aria-describedby="child-modal-description"
             >
                 <Box sx={{ ...style, width: 200 }}>
-                    <h2 id="child-modal-title">Text in a child modal</h2>
-                    <p id="child-modal-description">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    </p>
-                    <Button onClick={handleClose}>Close Child Modal</Button>
+                    <props.name/>   
                 </Box>
             </Modal>
+    }
+
+    return (
+        <>
+            <Button onClick={handleOpen}>Language Puzzle</Button>
+            <modal name="LanguageSelect"/>
+            <br/>
+            <Button onClick={handleOpen}>Polygon Puzzle</Button>
+            <br/>
+            <Button onClick={handleOpen}>Pattern Puzzle</Button>
+            <br/>
+            <Button onClick={handleOpen}>Image Shuffle</Button>
         </>
     );
 }
